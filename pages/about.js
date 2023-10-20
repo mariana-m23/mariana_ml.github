5c090fb57fab9b3879625f115d221150440b9161
mariana-m23 <147016788+mariana-m23@users.noreply.github.com>
2023-10-19 23:44:54 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -12,13 +12,14 @@ export default function About() {
         className="px-4"
       >
         <div className="mb-6 text-center text-gray-800 dark:text-white">
-          Hi, I'm Katherine! I am a Senior Software Engineer at GitHub with a passion for developing web and mobile
-          applications that make a positive impact on peoples lives.
+          Hi, I am Mariana Maricela Lozano! I am a Sophmore Computer Science Student studying at Texas A&M University - San Antonio. I have a passion for developing web 
+          applications and learning of new ways of incorporating the tech industry into the fashion industry. I am planning on becoming a Software Engineer, and I am up to learning
+            new skills and gaining further experience.
         </div>
         <div className="text-center text-gray-800 dark:text-white">
-          In addition to coding and learning new tech, I enjoy rock climbing, drinking coffee,
-          playing guitar, reading, and hanging with my dog. If any of these things interest you too,
-          I'd love to chat!
+          In addition to coding and learning new tech, I love to travel, read,
+          playing piano, work-out, and hanging with my dogs. I have a interest in photography too!
+          
         </div>
       </motion.div>
     </Layout>
